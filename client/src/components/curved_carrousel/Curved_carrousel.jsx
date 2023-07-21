6cd3c6de3b410fce64ef097ed5a2babe3faa96f3
ASWINKUMAR C M <aswin.trikkur@gmail.com>
2023-07-21 17:50:32 +0530

    carrousel slider updated

--- client/src/components/curved_carrousel/Curved_carrousel.jsx
+++ client/src/components/curved_carrousel/Curved_carrousel.jsx
@@ -2,12 +2,32 @@ import "./curved_carrousel.css";
 import React from "react";
 
 export const Curved_carrousel = () => {
+
+	const handleCarrouselSlider=(event,dir)=>{
+		// console.log(event.target.style);
+		// console.log(event.currentTarget.style);
+
+		let element= document.querySelector('.images')
+		// console.log(element);
+		// element.scroll({
+		// 	// top: 100,
+		// 	left: 100,
+		// 	// right:-1000,
+		// 	behavior: "smooth",
+		//   });
+		// console.log(element.scrollWidth);
+		element.scrollBy({
+			left: (dir==='right' ? 200 : -200),
+			behavior:"smooth",			
+		});
+	}
+
 	return (
 		<div className="carrousel-container">
-			<div></div>
 			<div className="image-container">
 					<div className="extruder extruder-top"></div>
-					<button> <i className="fa-solid fa-chevron-left"></i></button>
+					<button className="btn-left" onClick={(event)=>{handleCarrouselSlider(event,'left')}}> <i className="fa-solid fa-chevron-left " ></i></button>
+					<button className="btn-right" onClick={(event)=>{handleCarrouselSlider(event,'right')}}> <i className="fa-solid fa-chevron-right "></i></button>
 
 					<div className="images">
 						<div className="img1 img"><img src="./images/Frame 2.png" alt="" /></div>
@@ -23,7 +43,6 @@ export const Curved_carrousel = () => {
 						<div className="img4 img"><img src="./images/Frame 6.png" alt="" /></div>
 					</div>
 
-					<button> <i className="fa-solid fa-chevron-right"></i></button>
 					<div className="extruder extruder-bottom"></div>
 			</div>
 		</div>
